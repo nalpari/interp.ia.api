741cd12e135d850b2bceef75230e433322942626
DESKTOP-EJG0M5T\User <amekimm@gmail.com>
2025-02-28 14:29:00 +0900

    feat: issue 유형, 상태, 중요도 enum 추가

---
+++ src/main/java/net/devgrr/interp/ia/api/config/issue/Priority.java
@@ -0,0 +1,17 @@
+package net.devgrr.interp.ia.api.config.issue;
+
+import lombok.Getter;
+
+@Getter
+public enum Priority {
+  EMERGENCY("Emergency"), // 긴급
+  HIGH("High"), // 높음
+  MEDIUM("Medium"), // 중간
+  LOW("Low"); // 낮음
+
+  private final String description;
+
+  Priority(String description) {
+    this.description = description;
+  }
+}
