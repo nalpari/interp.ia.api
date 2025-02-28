411891eb205facf6326a57cce0b233bb6697fbd9
DESKTOP-EJG0M5T\User <amekimm@gmail.com>
2025-02-28 14:49:59 +0900

    refactor: BaseEntity 변수명 수정

--- src/main/java/net/devgrr/interp/ia/api/config/mapStruct/MemberMapper.java
+++ src/main/java/net/devgrr/interp/ia/api/config/mapStruct/MemberMapper.java
@@ -50,7 +50,7 @@ public interface MemberMapper {
   @Mapping(target = "image", ignore = true)
   @Mapping(target = "role", ignore = true)
   @Mapping(target = "isActive", ignore = true)
-  @Mapping(target = "createdAt", ignore = true)
+  @Mapping(target = "createdDate", ignore = true)
   @Mapping(target = "refreshToken", source = "refreshToken")
   Member updateMemberRefreshToken(Member updateMember, @MappingTarget Member member);
 }
