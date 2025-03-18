97e1dc8978552f5970498d89f85719daf8a87486
keyy1315 <lovelina1315@gmail.com>
2025-03-18 16:13:10 +0900

    feat: Spring Batch 관련 파일 제거

--- src/main/java/net/devgrr/interp/ia/api/InterpIaApiApplication.java
+++ src/main/java/net/devgrr/interp/ia/api/InterpIaApiApplication.java
@@ -1,13 +1,11 @@
 package net.devgrr.interp.ia.api;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableBatchProcessing
 public class InterpIaApiApplication {
 
   public static void main(String[] args) {
