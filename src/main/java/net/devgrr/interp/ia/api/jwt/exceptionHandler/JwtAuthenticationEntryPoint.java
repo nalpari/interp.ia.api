8698069123c58e5e34dec15fd72f5edb62d1281d
keyy1315 <lovelina1315@gmail.com>
2025-03-27 11:15:48 +0900

    refactor: import 구문 최적화

--- src/main/java/net/devgrr/interp/ia/api/jwt/exceptionHandler/JwtAuthenticationEntryPoint.java
+++ src/main/java/net/devgrr/interp/ia/api/jwt/exceptionHandler/JwtAuthenticationEntryPoint.java
@@ -2,13 +2,10 @@ package net.devgrr.interp.ia.api.jwt.exceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import java.io.IOException;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
