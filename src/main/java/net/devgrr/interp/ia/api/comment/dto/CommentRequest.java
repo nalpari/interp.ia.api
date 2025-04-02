24d96ceeaaa39dcb3b89ac64be11b3fe69e44442
keyy1315 <lovelina1315@gmail.com>
2025-04-02 17:12:32 +0900

    refactor: API Parameter 의 ReferenceType 을 String 에서 IssueCategory 타입으로 변경

--- src/main/java/net/devgrr/interp/ia/api/comment/dto/CommentRequest.java
+++ src/main/java/net/devgrr/interp/ia/api/comment/dto/CommentRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import net.devgrr.interp.ia.api.config.issue.IssueCategory;
 
 @Schema(description = "댓글 요청")
 public record CommentRequest(
@@ -13,7 +14,7 @@ public record CommentRequest(
     @Schema(description = "댓글이 달린 Entity 타입 (Issue/Project)")
         @NotNull(message = "필수값: reference type", groups = CommentValidationGroup.postGroup.class)
         @JsonView(CommentValidationGroup.postGroup.class)
-        String referenceType,
+        IssueCategory referenceType,
     @Schema(description = "댓글이 달린 Entity ID")
         @NotNull(message = "필수값: Entity id", groups = CommentValidationGroup.postGroup.class)
         @JsonView(CommentValidationGroup.postGroup.class)
--- src/main/java/net/devgrr/interp/ia/api/config/mapStruct/CommentMapper.java
+++ src/main/java/net/devgrr/interp/ia/api/config/mapStruct/CommentMapper.java
@@ -4,49 +4,31 @@ import java.util.List;
 import net.devgrr.interp.ia.api.comment.dto.CommentRequest;
 import net.devgrr.interp.ia.api.comment.dto.CommentResponse;
 import net.devgrr.interp.ia.api.comment.entity.Comment;
-import net.devgrr.interp.ia.api.config.exception.BaseException;
-import net.devgrr.interp.ia.api.config.exception.ErrorCode;
-import net.devgrr.interp.ia.api.config.issue.IssueCategory;
 import net.devgrr.interp.ia.api.member.entity.Member;
 import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
+  
+  @Mapping(source = "comment.member.email", target = "writerEmail")
+  @Mapping(source = "comment.member.name", target = "writerName")
+  CommentResponse toResponse(Comment comment);
 
-    @Named("toReferenceType")
-    static IssueCategory toReferenceType(String type) throws BaseException {
-        if (type == null) {
-            throw new BaseException(ErrorCode.INVALID_INPUT_VALUE, "참조 타입이 비어 있습니다. (issue/project)");
-        }
-        String r = type.toUpperCase();
-        if("ISSUE".equals(r)) {
-            return IssueCategory.ISSUE;
-        } else if("PROJECT".equals(r)) {
-            return IssueCategory.PROJECT;
-        } else {
-            throw new BaseException(ErrorCode.INVALID_INPUT_VALUE, "참조 타입이 잘못되었습니다. (issue/project)");
-        }
-    }
+  @Mapping(target = "id", ignore = true)
+  @Mapping(source = "member", target = "member")
+  Comment toComment(CommentRequest req, Member member);
 
-    @Mapping(source = "comment.member.email", target = "writerEmail")
-    @Mapping(source = "comment.member.name", target = "writerName")
-    CommentResponse toResponse(Comment comment);
+  @Mapping(source = "parentComment.member.email", target = "writerEmail")
+  @Mapping(source = "parentComment.member.name", target = "writerName")
+  @Mapping(source = "childComments", target = "childComment")
+  CommentResponse toResponseWithChildren(
+      Comment parentComment, List<CommentResponse> childComments);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(source = "member", target = "member")
-    @Mapping(source = "req.referenceType", target = "referenceType", qualifiedByName = "toReferenceType")
-    Comment toComment(CommentRequest req, Member member);
-
-    @Mapping(source = "parentComment.member.email", target = "writerEmail")
-    @Mapping(source = "parentComment.member.name", target = "writerName")
-    @Mapping(source = "childComments", target = "childComment")
-    CommentResponse toResponseWithChildren(Comment parentComment, List<CommentResponse> childComments);
-
-    @Mapping(target = "content", source = "req.content")
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "parentCommentId", ignore = true)
-    @Mapping(target = "member", ignore = true)
-    @Mapping(target = "referenceType", ignore = true)
-    @Mapping(target = "referenceId", ignore = true)
-    Comment updateComment(CommentRequest req, @MappingTarget Comment comment);
+  @Mapping(target = "content", source = "req.content")
+  @Mapping(target = "id", ignore = true)
+  @Mapping(target = "parentCommentId", ignore = true)
+  @Mapping(target = "member", ignore = true)
+  @Mapping(target = "referenceType", ignore = true)
+  @Mapping(target = "referenceId", ignore = true)
+  Comment updateComment(CommentRequest req, @MappingTarget Comment comment);
 }
