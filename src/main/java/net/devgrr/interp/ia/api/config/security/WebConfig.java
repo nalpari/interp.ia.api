c5cf9808b16afbf7dda20661b80d02682ac0783a
seulda <amekimm@gmail.com>
2025-04-03 11:00:02 +0900

    feat: cors 설정 추가

---
+++ src/main/java/net/devgrr/interp/ia/api/config/security/WebConfig.java
@@ -0,0 +1,18 @@
+package net.devgrr.interp.ia.api.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOriginPatterns("*")
+        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
+        .exposedHeaders("*")
+        .allowCredentials(true);
+  }
+}
